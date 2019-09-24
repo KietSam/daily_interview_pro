45c0679623adcba93a42111be2df3d3496edc227
Kiet Sam <kietmsm@gmail.com>
2019-09-24 12:22:35 -0700

    2019.9.24

---
+++ 2019.9.24/my_solution.py
@@ -0,0 +1,45 @@
+import unittest
+
+
+def helper(exp, start_index, end_index, paren_args):
+    if end_index < start_index:
+        return
+    if exp[start_index] == '+':
+        return + helper(exp, start_index + 1, end_index, paren_args)
+    if exp[start_index] == '-':
+        return - helper(exp, start_index + 1, end_index, paren_args)
+    if exp[start_index] == '(':
+        paren_args.append(None)
+        return helper(exp, start_index + 1, end_index, paren_args)
+    if exp[start_index] == ')':
+
+    if
+
+    pass
+
+
+def eval(expression):
+    # Fill this in.
+    exp_array = expression.split(" ")
+    print(exp_array)
+    return helper(exp_array, 0, len(exp_array) - 1, [])
+
+
+
+class BoilerplateTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test1(self):
+        # expected, actual
+        self.assertEqual(-4, eval('- ( 3 + ( 2 - 1 ) )'))
+        pass
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
